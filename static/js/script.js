4feba47cf074424179d9b0d5e627d4e37dff3d63
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-07-18 21:21:51 +0000

    intervale time

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,20 @@
 const button = document.querySelector('#searchButton');
 const loader = document.querySelector(".loader");
-
+let isRunning = false;
 
 button.addEventListener('click', (event)=>{
     event.preventDefault();
-    setInterval(
-            loader.style.display = 'block'
-    )
+
+    if(!isRunning){
+        isRunning = true;
+        loader.style.display = 'block';
+
+        setTimeout(()=>{
+            loader.style.display = 'none';
+            isRunning = false
+        }, 3000
+               
+        )
+    }
+   
 })
