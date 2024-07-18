59a8a42c3457aa81ce2ea7609563432f59e9042f
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-07-18 18:22:38 +0000

    add loader circular bar

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('#searchButton');
+const loader = document.querySelector(".loader");
+
+
+button.addEventListener('click', (event)=>{
+    event.preventDefault();
+    setInterval(
+            loader.style.display = 'block',1000
+    )
+})
