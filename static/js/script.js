4b61a2e279c304f2d60f40c58b7df1b2bc57e3dd
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-07-19 11:00:46 +0000

    resolve sycnchone probleme

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('#searchButton');
 const loader = document.querySelector(".loader");
+const table = document.querySelector('table')
 let isRunning = false;
 
 button.addEventListener('click', (event)=>{
@@ -8,9 +9,11 @@ button.addEventListener('click', (event)=>{
     if(!isRunning){
         isRunning = true;
         loader.style.display = 'block';
+        table.style.display = 'none'
 
         setTimeout(()=>{
             loader.style.display = 'none';
+            table.style.display = 'block'
             isRunning = false
         }, 3000
                
